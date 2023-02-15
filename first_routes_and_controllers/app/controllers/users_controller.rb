8a378259622e86c7345c056439c187d00c61ddf2
Tysuiku <Timothydong41@gmail.com>
2023-02-15 16:37:33 -0500

    finish phase 3

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-    def index
-        render plain: "I'm in the index action!"
-    end
+  def index
+    render plain: "I'm in the index action!"
+  end
 
+  def create
+    render json: params
+  end
 
+  def show
+    render json: params
+  end
 end
