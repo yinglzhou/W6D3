9652471505f8cb5a3e0ca72ecf3fe97c1856ade9
Tysuiku <Timothydong41@gmail.com>
2023-02-16 10:11:04 -0500

    finish controller

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,13 +1,43 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    @users = User.all
+    render json: @users
   end
 
   def create
-    render json: params
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
   end
 
   def show
-    render json: params
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_url(@user.id)
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: "User destroyed"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
