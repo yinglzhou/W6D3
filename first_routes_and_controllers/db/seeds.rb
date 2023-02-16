4e8734f427750303983925b11de98d3fab0497d8
Tysuiku <Timothydong41@gmail.com>
2023-02-16 13:59:32 -0500

    add seeds

--- first_routes_and_controllers/db/seeds.rb
+++ first_routes_and_controllers/db/seeds.rb
@@ -5,3 +5,74 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  puts "Preparing #{Rails.env} environment"
+
+  puts "Destroying tables..."
+  User.destroy_all
+  Artwork.destroy_all
+  ArtworkShare.destroy_all
+
+  puts "Resetting id sequences..."
+  %w(users artworks artwork_shares).each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+  end
+
+  puts "Creating seed data..."
+  tim = User.create!(
+    username: "tim",
+  )
+
+  ying = User.create!(
+    username: "ying",
+  )
+
+  shan = User.create!(
+    username: "shan",
+  )
+
+  gary = User.create!(
+    username: "gary",
+  )
+
+  city = Artwork.create!(
+    title: "city",
+    image_url: "cracked.org",
+    artist_id: 1,
+  )
+
+  monalisa = Artwork.create!(
+    title: "mona lisa",
+    image_url: "monalisa.org",
+    artist_id: 2,
+  )
+
+  starrynight = Artwork.create!(
+    title: "starrynight",
+    image_url: "starry.com",
+    artist_id: 2,
+  )
+
+  mirror1 = Artwork.create!(
+    title: "the mirror",
+    image_url: "self.com",
+    artist_id: 4,
+  )
+
+  mirror2 = Artwork.create!(
+    title: "the mirror",
+    image_url: "selfy.com",
+    artist_id: 2,
+  )
+
+  view1 = ArtworkShare.create!(
+    artwork_id: 3,
+    viewer_id: 4,
+  )
+
+  view2 = ArtworkShare.create!(
+    artwork_id: 3,
+    viewer_id: 3,
+  )
+  puts "Done with #{Rails.env} environment!"
+end
